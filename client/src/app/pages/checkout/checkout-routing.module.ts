6115e69f1548820e3c66d77d6bf092389a671677
Bruno Kelly <45316003+brunokelly@users.noreply.github.com>
2023-01-21 18:28:55 -0300

    end section checkout

--- client/src/app/pages/checkout/checkout-routing.module.ts
+++ client/src/app/pages/checkout/checkout-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
   {
     path: '',
-    data: {title: 'Basket' },
+    data: {title: 'Checkout' },
     component: CheckoutComponent,
   },
 ];
--- client/src/app/pages/checkout/checkout.module.ts
+++ client/src/app/pages/checkout/checkout.module.ts
@@ -2,8 +2,6 @@ import { CheckoiutRoutingModule } from './checkout-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
